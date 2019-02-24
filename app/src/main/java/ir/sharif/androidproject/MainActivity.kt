22607a2512b578919c5e2244a9a3a9c8993c29d1
Mahdi Hasanzadeh <m.hasan1395@student.sharif.edu>
2019-02-24 17:13:10 +0330

    fill main methods

--- app/src/main/java/ir/sharif/androidproject/MainActivity.kt
+++ app/src/main/java/ir/sharif/androidproject/MainActivity.kt
@@ -1,8 +1,12 @@
 package ir.sharif.androidproject
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import ir.sharif.androidproject.model.Item
+import ir.sharif.androidproject.repository.DataRepository
 import kotlinx.android.synthetic.main.activity_main.*
+import ir.sharif.androidproject.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +18,21 @@ class MainActivity : AppCompatActivity() {
         fetchButton.setOnClickListener { fillList() }
     }
 
-    private fun clearList() {}
-    private fun refreshList() {}
-    private fun fillList() {}
+    private fun clearList() {
+        DataRepository.clear()
+        list.removeAllViews()
+    }
+
+    private fun refreshList() {
+        DataRepository.refresh()
+        list.removeAllViews()
+        bindView(DataRepository.getData())
+    }
+
+    private fun fillList() =
+        bindView(DataRepository.fetchNewData())
+
+    private fun bindView(datas: ArrayList<Item>) =
+        datas.forEach { _ -> list.addView(LayoutInflater.from(this).inflate(R.layout.item_layout, list)) }
+
 }
--- app/src/main/java/ir/sharif/androidproject/repository/DataRepository.kt
+++ app/src/main/java/ir/sharif/androidproject/repository/DataRepository.kt
@@ -7,17 +7,29 @@ import ir.sharif.androidproject.model.Item
  *  @Date:   24/02/2019
  */
 
-val USERS = arrayListOf(
+val INITIAL_DATA = arrayListOf(
     Item("Mahdi Hasanzadeh", "Hi Bro!", ""),
     Item("Alireza Fatemi Jahromi", "I miss you...", ""),
     Item("Reza Zeiny", ":((((", "")
 )
 
 object DataRepository {
-    fun fetchData() = USERS
+    private var list = ArrayList<Item>()
 
-    fun generateNewData() {
-        for (i in USERS.size..USERS.size + 10) USERS.add(Item("Title$i", "SubTitle$i", ""))
-        USERS
+    fun fetchNewData(): ArrayList<Item> {
+        val newData = ArrayList<Item>()
+        for (i in list.size..list.size + 10) {
+            newData.add(Item("Title$i", "SubTitle$i", ""))
+        }
+        list.addAll(newData)
+        return newData
+    }
+
+    fun getData() = list
+
+    fun clear() = list.clear()
+
+    fun refresh() {
+        list = INITIAL_DATA
     }
 }
