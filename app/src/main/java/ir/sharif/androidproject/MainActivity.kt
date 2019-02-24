0cd2a04b06d08c5b90524a3029ea984607b770a3
m.hasanzadeh <m.hasanzadeh@mohaymen.co>
2019-02-24 16:13:43 +0330

    make ui

--- app/src/main/java/ir/sharif/androidproject/MainActivity.kt
+++ app/src/main/java/ir/sharif/androidproject/MainActivity.kt
@@ -1,12 +1,20 @@
 package ir.sharif.androidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clearButton.setOnClickListener { clearList() }
+        refreshButton.setOnClickListener { refreshList() }
+        fetchButton.setOnClickListener { fillList() }
     }
+
+    private fun clearList() {}
+    private fun refreshList() {}
+    private fun fillList() {}
 }
