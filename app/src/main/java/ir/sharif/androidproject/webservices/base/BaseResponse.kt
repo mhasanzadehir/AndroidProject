003b612e582b6dfeefdd1b576f8cecd1d87f21de
mahdihs76 <mahdihasanzadeh76@gmail.com>
2019-04-09 17:03:15 +0430

    change BaseResponse

--- app/src/main/java/ir/sharif/androidproject/webservices/base/BaseResponse.kt
+++ app/src/main/java/ir/sharif/androidproject/webservices/base/BaseResponse.kt
@@ -1,8 +1,3 @@
 package ir.sharif.androidproject.webservices.base
 
-import com.google.gson.annotations.Expose
-
-open class BaseResponse {
-    @Expose
-    var messageBody: String? = null
-}
+open class BaseResponse
