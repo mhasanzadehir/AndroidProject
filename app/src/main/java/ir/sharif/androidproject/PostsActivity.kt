9ecc8ad81b8e002bffc857c5bbbde802d67b07e4
Reza Zeiny <rezazeiny1998@gmail.com>
2019-04-27 02:23:43 +0430

    Some change in UI

--- app/src/main/java/ir/sharif/androidproject/PostsActivity.kt
+++ app/src/main/java/ir/sharif/androidproject/PostsActivity.kt
@@ -1,5 +1,9 @@
 package ir.sharif.androidproject
 
+import android.app.AlertDialog
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
@@ -13,6 +17,10 @@ import ir.sharif.androidproject.models.AdvertisementType
 import ir.sharif.androidproject.webservice.webservices.posts.PostResponse
 import kotlinx.android.synthetic.main.activity_posts.*
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.util.Log
+import java.util.logging.Logger
 
 
 class PostsActivity : AppCompatActivity(), Advertiser.AdvertiseListener<List<PostResponse>> {
@@ -23,11 +31,12 @@ class PostsActivity : AppCompatActivity(), Advertiser.AdvertiseListener<List<Pos
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
         setSupportActionBar(postsToolbar)
-        title = "prj2"
+        title = "Post Page"
         postList.layoutManager = LinearLayoutManager(this)
+//        postList.margin
         postAdapter = PostAdapter(arrayListOf())
         postList.adapter = postAdapter
-        MessageController.fetchPosts()
+        tryToConnect()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -47,6 +56,28 @@ class PostsActivity : AppCompatActivity(), Advertiser.AdvertiseListener<List<Pos
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        when(item?.itemId){
+            R.id.Zeiny -> dialogBuilder.setMessage("Reza Zeiny 95 CE Sharif University of Technology.")
+            R.id.Fatemi -> dialogBuilder.setMessage("Ali Reza Fatemi 95 CE Sharif University of Technology.")
+            R.id.Hassanzadeh -> dialogBuilder.setMessage("Mahdi Hassanzade 95 CE Sharif University of Technology.")
+            R.id.Mahdipour -> dialogBuilder.setMessage("Amir Hossein Mahdipour 95 CE Sharif University of Technology.")
+            else -> return super.onOptionsItemSelected(item)
+        }
+        dialogBuilder.setCancelable(true)
+        dialogBuilder.setPositiveButton("OK") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        val alert = dialogBuilder.create()
+        alert.setTitle("Person Info")
+        alert.show()
+
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onResume() {
         super.onResume()
         Advertiser.subscribe(this, AdvertisementType.POSTS_LOADED)
@@ -59,8 +90,74 @@ class PostsActivity : AppCompatActivity(), Advertiser.AdvertiseListener<List<Pos
 
     override fun receiveData(advertisement: Advertisement<List<PostResponse>>) {
         runOnUiThread {
+            Progressbar.visibility = View.GONE
             postAdapter.replaceData(advertisement.data)
         }
+    }
+
+    private fun isOnline(): Boolean {
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = connMgr.activeNetworkInfo
+        return networkInfo?.isConnected == true
+    }
+
+    private fun tryToConnect() {
+        if (isOnline()) {
+            runOnUiThread {
+                Progressbar.visibility = View.VISIBLE
+            }
+            MessageController.fetchPosts()
+        } else {
+            showNetworkAlert()
+        }
+    }
+
+    private fun readFromCache() {
+        //todo implement
+//        if (false){
+//            MessageController.fetchPosts()
+//        }
+//        else{
+        showCacheAlert()
+//        }
+    }
+
+    private fun showNetworkAlert() {
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        dialogBuilder.setMessage("No internet connection.")
+            .setCancelable(false)
+            .setPositiveButton("Continue without internet") { _, _ ->
+                readFromCache()
+            }
+            .setNegativeButton("try again for connect") { _, _ ->
+                tryToConnect()
+            }.setNeutralButton("exit programme") { _, _ ->
+                finish()
+            }
+
+
+        val alert = dialogBuilder.create()
+        alert.setTitle("Network Manager")
+        alert.show()
+    }
+
+    private fun showCacheAlert() {
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        dialogBuilder.setMessage("You have not any saved data or your data have has been expired.")
+            .setCancelable(false)
+            .setPositiveButton("Try to connect internet") { _, _ ->
+                tryToConnect()
+            }
+            .setNegativeButton("exit programme") { _, _ ->
+                finish()
+            }
+
+
+        val alert = dialogBuilder.create()
+        alert.setTitle("Cache Manager")
+        alert.show()
     }
 
 
