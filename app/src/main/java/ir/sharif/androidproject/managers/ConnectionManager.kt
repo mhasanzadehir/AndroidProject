6fb888a39a88cfd6fbc03188ca63688f974eb64a
alireza <alirezafa@rocketmail.com>
2019-04-26 13:43:35 +0430

    :card_file_box: Database is working like a charm.

--- app/src/main/java/ir/sharif/androidproject/managers/ConnectionManager.kt
+++ app/src/main/java/ir/sharif/androidproject/managers/ConnectionManager.kt
@@ -2,6 +2,8 @@ package ir.sharif.androidproject.managers
 
 import ir.sharif.androidproject.Advertiser
 import ir.sharif.androidproject.MessageController
+import ir.sharif.androidproject.MyApplication
+import ir.sharif.androidproject.database.posts.PostBean
 import ir.sharif.androidproject.models.Advertisement
 import ir.sharif.androidproject.models.AdvertisementType
 import ir.sharif.androidproject.models.Item
@@ -13,6 +15,12 @@ object ConnectionManager {
     fun loadPosts() {
         cloud.postRunnable {
             val posts = WebserviceHelper.getPosts()
+            MyApplication.database.postDao().nukeTable()
+            MyApplication.database.postDao().insertAll(*posts.map { postResponse ->
+                with(postResponse) {
+                    PostBean(id!!, userId!!, title, body)
+                }
+            }.toTypedArray())
             Advertiser.advertise(Advertisement(AdvertisementType.POSTS_LOADED, posts))
         }
     }
